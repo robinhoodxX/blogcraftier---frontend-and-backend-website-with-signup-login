708ed2fcdc83bca343d7c4a854427d435e51a93d
Haider Zaman Khan <59317216+robinhoodxX@users.noreply.github.com>
2025-12-10 12:08:18 +0500

    corrected module

--- src/components/hmpg/navbar/comp/theme.jsx
+++ src/components/hmpg/navbar/comp/theme.jsx
@@ -1,5 +1,5 @@
 import InvertColorsIcon from '@mui/icons-material/InvertColors';
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 function Theme() {
   return (
